648bd157254978334ece78081ba4a79658088117
a197382176 <a197382176@gmail.com>
2025-05-28 19:30:55 +0800

    Update build.sh

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -8,50 +8,43 @@ echo "Building for ROOTFS_PARTSIZE: $ROOTSIZE"
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
+
 # 定义所需安装的包列表
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-
-# 简体中文包（保留不变）
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+# 服务——FileBrowser 用户名admin 密码admin
 PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+
+#24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+# PACKAGES="$PACKAGES luci-app-openclash"
+# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+
+# 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
-# 新增繁體中文包
-PACKAGES="$PACKAGES luci-i18n-base-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-package-manager-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-tw"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-tw"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
-    # 简体 Docker 管理界面
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-    # 繁體 Docker 管理界面
-    PACKAGES="$PACKAGES luci-i18n-dockerman-zh-tw"
-    echo "Adding package: luci-i18n-dockerman-zh-cn and luci-i18n-dockerman-zh-tw"
+    echo "Adding package: luci-i18n-dockerman-zh-cn"
 fi
+
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE \
-           PACKAGES="$PACKAGES" \
-           FILES="/home/build/immortalwrt/files" \
-           ROOTFS_PARTSIZE=$ROOTSIZE
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTSIZE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
